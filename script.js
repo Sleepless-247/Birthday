0a82185973a3b926aa00e1626d6ac126afd77ed0
Sleepless-247 <lukewhalen707@gmail.com>
2025-03-23 11:52:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function handleAgeChoice(choice) {
   if (choice === 'yes') {
     document.getElementById('topMessage').innerHTML = 
   "<p>Okay... if you say so grandma. I guess I'll let you go to the next page. Don't fall over on the way there tho.</p>" +
-  "<img src='https://static.wikia.nocookie.net/garfield/images/c/c9/GarfieldOlderandWiderCover.png/revision/latest/thumbnail/width/360/height/450?cb=20181006065119' alt='Funny grandma' style='width: 200px; display: block; margin-top: 10px;'>";
+  "<img src='https://m.media-amazon.com/images/I/71RtJXBTNJL._UF350,350_QL50_.jpg' alt='Funny grandma' style='width: 400px; display: block; margin-top: 10px;'>";
 
 
     document.getElementById('ageChoice').style.display = 'none'; // Hide Yes/No buttons
